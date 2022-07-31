c03dd3f10188a034ec33582e8e7bfef2be080474
taka <shimakuratakanao@gmail.com>
2022-07-31 19:12:04 +0900

    add 404 page

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,7 +1,38 @@
+import './NotFound.css'
+import React from 'react';
+import Lottie from 'react-lottie';
+import * as animationData from './daddy.json';
 export const NotFound = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+    /*
     return (
-        <div>
-            お探しのページは見当たりません
+        <div className="notFoundmain">
+            <h1>404</h1>
+            <h2>お探しのページは見当たりません</h2>
+        </div>
+    )
+    */
+    return (
+        <div className="notFoundmain">
+            <Lottie options={defaultOptions} height={200} width={200} />
+            <h1>404</h1>
+            <h2>お探しのページは見当たりません</h2>
         </div>
     );
+
 };
+
+/*
+<div className="notFoundmain">
+            <h1>404</h1>
+            <h2>お探しのページは見当たりません</h2>
+        </div>
+*/
+
